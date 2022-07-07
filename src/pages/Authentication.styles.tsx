e0a8fd0e2f67208c8d54ba5531fb2d841412dced
SamuelCGP <69848773+SamuelCGP@users.noreply.github.com>
2022-07-06 21:07:29 -0300

    Funny menu slide & color palette

--- src/pages/Authentication.styles.tsx
+++ src/pages/Authentication.styles.tsx
@@ -1,8 +1,43 @@
 import React from "react";
 import StyleSheet from "../utils/StyleSheet";
+import ColorPalette from "../utils/ColorPalette";
 
-const styles: StyleSheet = {
-    container: {
-        backgroundColor: 'red',
+const AuthStyles: StyleSheet = {
+    authentication: {
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: ColorPalette.darkBlue,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
+    },
+    title: {
+        color: ColorPalette.white,
+        fontFamily: 'verdana',
+        textAlign: 'center',
+    },
+    mainContainer: {
+        width: '80vw',
+        height: '70vh',
+        backgroundColor: ColorPalette.navyBlue,
+        borderRadius: '30px'
+    },
+    loginCard: {
+        width: '30%',
+        height: '100%',
+        backgroundColor: ColorPalette.white,
+        borderRadius: '30px',
+        transition: '1s'
+    },
+    loginCardHover: {
+        width: '30%',
+        height: '100%',
+        backgroundColor: ColorPalette.white,
+        borderRadius: '30px',
+        marginLeft: '70%',
+        transition: '1s'
     }
 };
+
+export default AuthStyles;
