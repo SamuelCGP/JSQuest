14f8f989fa83ce3bcb36820acf98967f8a70f80a
SamuelCGP <69848773+SamuelCGP@users.noreply.github.com>
2022-07-14 17:58:10 -0300

    communication tests

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+function Home() {
+    const [response, setResponse] = React.useState('');
+    const serverURL = 'http://localhost:3001/';
+    let testRES: any;
+
+    useEffect(() => {
+        console.log(testRES);
+    })
+
+    const options = {
+        url: 'http://localhost:3001/user/login',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8'
+        },
+        data: {
+          email: 'test',
+          password: 'test'
+        }
+    };
+
+    axios(options) // retorna um objeto
+    //.post(`${serverURL}user/login`, JSON.stringify({ email: 'test', password: 'test' }))
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+    return ( 
+        <div style={ {color: 'white', fontSize: '20px', padding: '20px'} }>
+            <h1>This is a test</h1>
+            <p>{response}</p>
+        </div> 
+    );
+}
+
+export default Home;
