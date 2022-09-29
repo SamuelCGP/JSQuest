282ddb41978ec643361fde51632ddd23f2236e6b
samuelcgp <cgplays51@gmail.com>
2022-09-29 11:01:36 -0300

    robot movement 29/09

--- client/src/components/ExerciseLesson/BoardElements/Robot/Robot.tsx
+++ client/src/components/ExerciseLesson/BoardElements/Robot/Robot.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { BoardElementProps } from "../BoardElementProps";
 
 export function Robot(props: BoardElementProps) {
+	const [x, setX] = useState(props.positionX);
+	const [y, setY] = useState(props.positionY);
+
 	const RobotSymbol = styled.div`
+		
 		background-color: red;
 		width: 100%;
 		height: 100%;
 		border-radius: 100%;
-		grid-column: ${props.positionX};
-		grid-row: ${props.positionY};
+		grid-column: ${x};
+		grid-row: ${y};
 	`;
 
 	const changePosition = (evt: any) => {
 		console.log(evt.target.getBoundingClientRect())
+		moveTo(2,2);
+		console.log(evt.target.getBoundingClientRect())
+	}
+
+	const moveTo = (newX: number, newY: number) => {
+		setX(newX);
+		setY(newY);
 	}
 
 	return ( <RobotSymbol onClick={changePosition}/> );
