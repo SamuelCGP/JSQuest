b60050cfea3588841387d4cdda2684009ef428e9
SamuelCGP <69848773+SamuelCGP@users.noreply.github.com>
2022-10-30 16:47:36 -0300

    demonstração

--- client/src/game/runCode.tsx
+++ client/src/game/runCode.tsx
@@ -1,5 +1,6 @@
 import { verifySolution } from "../api/solution";
-import { robotMethods } from "./robotMethods";
+import { andar, randomId } from "./robotMethods";
+import * as signals from "./signals";
 
 export const runCode = (
 	chapterIndex: number | any,
@@ -8,20 +9,43 @@ export const runCode = (
 ) => {
 	const runRes: any = verifySolution(chapterIndex, lessonIndex, code).then(
 		(res) => {
+			switch (res.status) {
+				case 200:
+					handleSolutionSuccess(res);
+					break;
+				default:
+					handleSolutionFailure(res);
+					break;
+			}
 			return res;
 		}
 	);
-
-	switch (runRes.status) {
-		case 400:
-			handleSolutionFailure(runRes);
-			break;
-		case 200:
-			handleSolutionSuccess(runRes);
-			break;
-	}
 };
 
-const handleSolutionFailure = (res: any) => {};
+const handleSolutionFailure = (res: any) => {
+	console.log(res);
+};
 
-const handleSolutionSuccess = (res: any) => {};
+const handleSolutionSuccess = (res: any) => {
+	console.log(res);
+
+	let codeToExec = res.data.codeToExec ? res.data.codeToExec : null;
+	if (codeToExec !== "" && codeToExec !== null) {
+		const splitedCode = codeToExec.split(";");
+		console.log("split", splitedCode);
+
+		const context =
+			`const _signals__WEBPACK_IMPORTED_MODULE_0__ = {fireSignal: ${signals.fireSignal}};` +
+			`const andar = ${andar};` +
+			`const randomId = ${randomId};`;
+
+		codeToExec =
+			context +
+			`setTimeout(() => {${splitedCode[0]}}, 1000);` +
+			`setTimeout(() => {${splitedCode[1]}}, 2000);` +
+			`setTimeout(() => {${splitedCode[2]}}, 3000);`;
+		console.log(codeToExec);
+		eval(codeToExec);
+	} else {
+	}
+};
--- src/exercisesContext.ts
+++ src/exercisesContext.ts
@@ -1,51 +1,51 @@
 export interface ContextObj {
-    _dialog: string[],
-    _codeString: string,
-    _yPos: number,
-    _xPos: number,
-    andar(direcao: string): void
-    console: {
-        log(message: any): void
-    },
-    [key: string]: any
+	_dialog: string[];
+	_codeString: string;
+	_yPos: number;
+	_xPos: number;
+	andar(direcao: string): void;
+	console: {
+		log(message: any): void;
+	};
+	[key: string]: any;
 }
 
 export class Context {
-    constructor() {}
-    
-    contextObj: ContextObj = {
-        _dialog: [] as string[],
-        _codeString: "",
-        _yPos: 0,
-        _xPos: 0,
-        
-        andar: (direcao: string) => {
-            this.contextObj._codeString += `andar(${direcao});`
+	constructor() {}
 
-            switch(direcao) {
-                case "cima":
-                    this.contextObj._yPos += 1;
-                    break;
-                case "baixo":
-                    this.contextObj._yPos -= 1;
-                    break;
-                case "esquerda":
-                    this.contextObj._xPos -= 1;
-                    break;
-                case "direita":
-                    this.contextObj._xPos += 1;
-                    break;
-                default:
-                    throw Error("Direção inválida");
-            }
-        },
-        console: {
-            log: (...messages: any) => {
-                for(let message of messages) {
-                    this.contextObj._codeString += `dialog("${message}");`;
-                    this.contextObj._dialog.push(`${message}`);
-                }
-            }
-        }
-    }
+	contextObj: ContextObj = {
+		_dialog: [] as string[],
+		_codeString: "",
+		_yPos: 0,
+		_xPos: 0,
+
+		andar: (direcao: string) => {
+			this.contextObj._codeString += `andar("${direcao}");`;
+
+			switch (direcao) {
+				case "cima":
+					this.contextObj._yPos += 1;
+					break;
+				case "baixo":
+					this.contextObj._yPos -= 1;
+					break;
+				case "esquerda":
+					this.contextObj._xPos -= 1;
+					break;
+				case "direita":
+					this.contextObj._xPos += 1;
+					break;
+				default:
+					throw Error("Direção inválida");
+			}
+		},
+		console: {
+			log: (...messages: any) => {
+				for (let message of messages) {
+					this.contextObj._codeString += `dialog("${message}");`;
+					this.contextObj._dialog.push(`${message}`);
+				}
+			},
+		},
+	};
 }
