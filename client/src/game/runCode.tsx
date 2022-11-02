f913677cd0caa2179de111f98baa23da961150d4
SamuelCGP <69848773+SamuelCGP@users.noreply.github.com>
2022-11-02 09:53:47 -0300

    Update runCode.tsx

--- client/src/game/runCode.tsx
+++ client/src/game/runCode.tsx
@@ -3,7 +3,7 @@ import { gameMethods } from "./gameMethods";
 import * as signals from "./signals";
 
 export interface Command {
-	[key: string]: any[]
+	[key: string]: any[];
 }
 
 export const runCode = (
@@ -11,23 +11,22 @@ export const runCode = (
 	lessonIndex: number | any,
 	code: any
 ) => {
-	verifySolution(chapterIndex, lessonIndex, code).then(
-		(res) => {
-			switch (res.status) {
-				case 200:
-					handleSolutionSuccess(res);
-					break;
-				default:
-					handleSolutionFailure(res);
-					break;
-			}
-			return res;
+	verifySolution(chapterIndex, lessonIndex, code).then((res) => {
+		switch (res.status) {
+			case 200:
+				handleSolutionSuccess(res);
+				break;
+			default:
+				handleSolutionFailure(res);
+				break;
 		}
-	);
+		return res;
+	});
 };
 
 const handleSolutionFailure = (res: any) => {
 	console.log(res);
+	alert(res.data.message);
 };
 
 const handleSolutionSuccess = (res: any) => {
@@ -37,31 +36,11 @@ const handleSolutionSuccess = (res: any) => {
 	const interval = 500;
 
 	commands.forEach((command, index) => {
-		setTimeout(function() {
+		setTimeout(function () {
 			const commandName: string = Object.keys(command)[0];
 			const args: any[] = command[commandName];
 
 			gameMethods[commandName](...args);
-		}, index * interval)
-	})
-
-
-	// if (codeToExec !== "" && codeToExec !== null) {
-	// 	const splitedCode = codeToExec.split(";");
-	// 	console.log("split", splitedCode);
-
-	// 	const context =
-	// 		`const _signals__WEBPACK_IMPORTED_MODULE_0__ = {fireSignal: ${signals.fireSignal}};` +
-	// 		`const andar = ${andar};` +
-	// 		`const randomId = ${randomId};`;
-
-	// 	codeToExec =
-	// 		context +
-	// 		`setTimeout(() => {${splitedCode[0]}}, 1000);` +
-	// 		`setTimeout(() => {${splitedCode[1]}}, 2000);` +
-	// 		`setTimeout(() => {${splitedCode[2]}}, 3000);`;
-	// 	console.log(codeToExec);
-	// 	eval(codeToExec);
-	// } else {
-	// }
+		}, index * interval);
+	});
 };
