a3e5df964428a3253961fb28078e70f450415442
SamuelCGP <69848773+SamuelCGP@users.noreply.github.com>
2022-11-08 22:18:19 -0300

    Theorical Lesson conclusion button

--- client/src/pages/Chapter/TheoricalLesson/TheoricalLesson.tsx
+++ client/src/pages/Chapter/TheoricalLesson/TheoricalLesson.tsx
@@ -1,8 +1,16 @@
-import { MainContainer, MainCard, ContentCard } from "./TheoricalLessonStyles";
+import {
+	MainContainer,
+	MainCard,
+	ContentCard,
+	FinishButton,
+	Blank,
+} from "./TheoricalLessonStyles";
+import { Popup } from "../../../components";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import rehypeHighlight from "rehype-highlight";
+import { finishGame } from "../../../game/finishGame";
 
 interface TheoricalLessonProps {
 	lessonData: any;
@@ -12,19 +20,26 @@ function TheoricalLesson(props: TheoricalLessonProps) {
 	const text = props.lessonData.lesson.text.split("\\");
 
 	return (
-		<MainContainer>
-			<MainCard>
-				<ContentCard>
-					{text.map((p: string) => (
-						<ReactMarkdown
-							children={p}
-							remarkPlugins={[gfm]}
-							rehypePlugins={[rehypeRaw, rehypeHighlight]}
-						/>
-					))}
-				</ContentCard>
-			</MainCard>
-		</MainContainer>
+		<>
+			<Popup type={"completion"} />
+			<MainContainer>
+				<MainCard>
+					<ContentCard>
+						{text.map((p: string) => (
+							<ReactMarkdown
+								children={p}
+								remarkPlugins={[gfm]}
+								rehypePlugins={[rehypeRaw, rehypeHighlight]}
+							/>
+						))}
+						<FinishButton onClick={finishGame}>
+							Concluir
+						</FinishButton>
+						<Blank />
+					</ContentCard>
+				</MainCard>
+			</MainContainer>
+		</>
 	);
 }
 
--- client/src/pages/Chapter/TheoricalLesson/TheoricalLessonStyles.tsx
+++ client/src/pages/Chapter/TheoricalLesson/TheoricalLessonStyles.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import ColorPalette from "../../../utils/ColorPalette";
 
 export const MainContainer = styled.div`
 	width: 100vw;
@@ -35,4 +36,31 @@ export const ContentCard = styled.div`
 	@media (min-width: 900px) {
 		width: 70%;
 	}
+	position: relative;
+
+	h1 {
+		text-align: center;
+	}
+`;
+
+export const FinishButton = styled.button`
+	outline: none;
+	border: none;
+	background-color: ${ColorPalette.primary};
+	color: white;
+	padding: 0.7rem;
+	border-radius: 10px;
+	margin-top: 1rem;
+	left: 50%;
+	position: relative;
+	cursor: pointer;
+	transform: translate(-50%);
+	&:hover {
+		filter: brightness(1.1);
+	}
+`;
+
+export const Blank = styled.div`
+	width: 100%;
+	height: 1rem;
 `;
